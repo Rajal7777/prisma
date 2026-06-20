558b5afb888d6107994dd59ed67d337167f9a90e
Rajal7777 <suwalrajal57@gmail.com>
2026-06-20 15:51:01 +0900

    initial setup

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+});
+
