760ed7378ba6082f7af9bf37ef8f187a01fda9d0
Rajal7777 <suwalrajal57@gmail.com>
2026-06-21 18:07:15 +0900

    update user data

--- index.ts
+++ index.ts
@@ -7,15 +7,26 @@ const app = express();
 app.use(express.json());
 
 
-app.use('/user', async (_, res) => {
-  const users = await prisma.user.findMany({
-    where: {
-    nationality: {
-      in: ['German', 'Indian', 'Japanese']
+// app.use('/user', async (_, res) => {
+//   const users = await prisma.user.findMany({
+//     where: {
+//     nationality: {
+//       in: ['German', 'Indian', 'Japanese']
+//     }
+//     }
+//  });
+//   res.json(users);
+// });
+
+app.put('/user', async (_, res) => {
+  const updatedUser = await prisma.user.update({
+    where: {email: "john.smith@example.com" },
+    data: {
+      name: "Jhon Changed",
+      nationality: "Findland"
     }
-    }
- });
-  res.json(users);
+  });
+  res.json(updatedUser);
 });
 
 const port = 4000;
